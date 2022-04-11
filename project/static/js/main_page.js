00ca046785c22fa70bbafcbc9d3b45ee67f97999
alexey F <52556121+fedenko03@users.noreply.github.com>
2022-04-11 16:15:39 +0600

    Changing the project structure (added separation of "static" into folders "js" and "images"

---
+++ project/static/js/main_page.js
@@ -0,0 +1,17 @@
+$( document ).ready(function(){
+	  $( "#el1" ).hover(function(){
+	    $( "#el1" ).css( "color", "#0d6efd" )
+	    }, function(){
+	    $( "#el1" ).css( "color", "#212529" )
+	  });
+	  $( "#el2" ).hover(function(){
+	    $( "#el2" ).css( "color", "#0d6efd" )
+	    }, function(){
+	    $( "#el2" ).css( "color", "#212529" )
+	  });
+	  $( "#el3" ).hover(function(){
+	    $( "#el3" ).css( "color", "#0d6efd" )
+	    }, function(){
+	    $( "#el3" ).css( "color", "#212529" )
+	  });
+	});
