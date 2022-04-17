dcdcf9926b42e2f13e8eda77d28858f8fa3b9def
alexey F <52556121+fedenko03@users.noreply.github.com>
2022-04-17 19:50:09 +0600

    Starting work on the registration/authorization page

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',  main, name="main")
+    path('',  main, name="main"),
+    path('login/', login, name="login")
 ]
--- project/views.py
+++ project/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import redirect, render, HttpResponse
 
 def main(request):
     return render(request, 'main.html')
+
+def login(request):
+    return render(request, 'login.html')
+
