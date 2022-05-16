e6d868b09b32b330249a7e281f2bfb982b399421
alexey F <52556121+fedenko03@users.noreply.github.com>
2022-05-16 19:57:27 +0600

    redesign for places page

--- project/admin.py
+++ project/admin.py
@@ -7,5 +7,5 @@ admin.site.register(Profile)
 
 @admin.register(Places)
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ("name", "type", "city")
+    list_display = ("name", "type", "city", "price")
     search_fields = ("name__startswith",)
